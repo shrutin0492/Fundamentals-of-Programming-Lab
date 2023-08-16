31f41c7edd9161de51275db2155776fc70bd6363
Shruti N <97854266+shrutin567@users.noreply.github.com>
2023-08-16 23:42:56 +0530

    Add files via upload

---
+++ 16_bubble sort_const array arguments.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+// Function to perform Bubble Sort on an array
+void bubbleSort(int arr[], const int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements if they are in the wrong order
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to display an array
+void displayArray(const int arr[], const int size) {
+    printf("Sorted Array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Perform Bubble Sort
+    bubbleSort(arr, size);
+
+    // Display the sorted array
+    displayArray(arr, size);
+
+    return 0;
+}
